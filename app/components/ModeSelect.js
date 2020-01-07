96b67cb50ccb6875aabba879ee520a8ec4310e96
James Shen <jjss886@gmail.com>
2020-01-07 16:57:44 -0500

    Mode functionality and styling working

--- app/components/ModeSelect.js
+++ app/components/ModeSelect.js
@@ -3,23 +3,24 @@ import styled from "styled-components";
 
 const StyledModeSelect = styled.div`
   box-sizing: border-box;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   margin: 0 0 20px 0;
   padding: 20px;
+  border: 4px solid #333;
   min-height: 30px;
   width: 100%;
   border-radius: 20px;
-  border: none;
-  color: white;
-  background: #333;
+  color: #999;
+  background: #000;
   font-family: Arial, Helvetica, sans-serif;
-  font-size: 1rem;
-  outline: none;
-  cursor: pointer;
+  font-size: 0.9rem;
 `;
 
 const ModeSelect = ({ setModeHandler }) => (
   <StyledModeSelect>
-    <span className="selectText">Mode: </span>
+    <span className="selectText">Mode:</span>
     <select className="selectDropdown" onChange={setModeHandler}>
       <option>Easy</option>
       <option>Medium</option>
