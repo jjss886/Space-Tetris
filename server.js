52b817e92f3a2b6919f167a2d9e27ab8898caee0
James Shen <jjss886@gmail.com>
2020-02-16 15:05:41 -0500

    setting up deploy mechanics with heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
